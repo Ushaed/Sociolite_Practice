c37a74a0884904a3187edc9e4527ed433128ae45
Md Ushaed <mdushaed29@gmail.com>
2020-02-19 02:06:32 +0600

    socialite added

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <h2><b>Login</b></h2>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            @if(Session::has('message'))
+                <div class="alert alert-{{Session::get('type')}}">{{ Session::get('message') }}</div>
+            @endif
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+
+                    </div>
+                    @error('email')
+                    <div class="text-danger"><strong>{{ $message }}</strong></div>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Password" name="password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    @error('password')
+                    <div class="text-danger"><strong>{{ $message }}</strong></div>
+                    @enderror
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <p class="mb-1">
+                <a href="#">I forgot my password</a>
+            </p>
+            <br />
+            <p class="text-center text-bold">OR</p>
+            <br />
+            <div class="form-group text-center">
+                <div class="">
+                    <a href="{{url('/auth/redirect/facebook')}}" class="btn btn-primary">Login with Facebook</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.login-card-body -->
+    </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+</body>
+
+</html>
